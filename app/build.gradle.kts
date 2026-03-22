f2446de14007798cd8f5e805c93a4c222103588b
didac <didacgasulla@gmail.com>
2026-03-22 13:48:35 +0100

    Merge main into MPV1-Crear-Llista-Events

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
-    id("com.google.gms.google-services")  // comillas dobles y paréntesis
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "edu.ub.pis2526.projecte"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "edu.ub.pis2526.projecte"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -36,14 +36,27 @@ android {
 }
 
 dependencies {
+    // Llibreries base d'Android
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.recyclerview)
     implementation(libs.cardview)
-    implementation("com.google.firebase:firebase-firestore:25.1.1")  // Firebase
+
+    // --- CONFIGURACIÓ FIREBASE ---
+    // El BoM gestiona les versions automàticament per a totes les llibreries de Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
+
+    // No cal posar versions aquí perquè ja les gestiona el BoM
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database")  // Realtime Database
+    implementation("com.google.firebase:firebase-firestore") // Cloud Firestore
+
+    // --- ALTRES ---
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // Tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)

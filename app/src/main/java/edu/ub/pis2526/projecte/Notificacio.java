14cf25ed1a63f3cf96eaf13a855133b76a84c768
Quim Vila Roset <qvilaros50@alumnes.ub.edu>
2026-04-29 12:16:22 +0200

    Classe Notificacio.java

---
+++ app/src/main/java/edu/ub/pis2526/projecte/Notificacio.java
@@ -0,0 +1,69 @@
+package edu.ub.pis2526.projecte;
+
+public class Notificacio {
+    private String id;
+    private String destinatari; // El nom o correu de l'usuari que ha de rebre l'avís
+    private String titol;
+    private String missatge;
+    private long timestamp;     // Per poder-les ordenar de més nova a més vella
+    private boolean llegida;    // Per saber si hem de posar el text en negreta o no
+
+    // Constructor buit obligatori per a Firestore
+    public Notificacio() {}
+
+    public Notificacio(String destinatari, String titol, String missatge) {
+        this.destinatari = destinatari;
+        this.titol = titol;
+        this.missatge = missatge;
+        this.timestamp = System.currentTimeMillis();
+        this.llegida = false;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitol() {
+        return titol;
+    }
+
+    public void setTitol(String titol) {
+        this.titol = titol;
+    }
+
+    public String getDestinatari() {
+        return destinatari;
+    }
+
+    public void setDestinatari(String destinatari) {
+        this.destinatari = destinatari;
+    }
+
+    public String getMissatge() {
+        return missatge;
+    }
+
+    public void setMissatge(String missatge) {
+        this.missatge = missatge;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isLlegida() {
+        return llegida;
+    }
+
+    public void setLlegida(boolean llegida) {
+        this.llegida = llegida;
+    }
+}

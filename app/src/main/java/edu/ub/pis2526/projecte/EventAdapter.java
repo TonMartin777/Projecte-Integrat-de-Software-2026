6cbee9d78beba572ec73e7af6b0355201ddb9fff
didacgasulla <didacgasulla@gmail.com>
2026-05-07 10:36:18 +0200

    aforo maxim corregit

--- app/src/main/java/edu/ub/pis2526/projecte/EventAdapter.java
+++ app/src/main/java/edu/ub/pis2526/projecte/EventAdapter.java
@@ -93,6 +93,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHol
             intent.putExtra("descripcion", event.getDescripcion());
             intent.putExtra("eventoId", event.getId());
             intent.putExtra("NOM_USUARI", nomUsuari);
+            intent.putExtra("aforoMaximo", event.getAforoMaxim());
             if (event.getFechaHora() != null) {
                 intent.putExtra("fecha", event.getFechaHora().toLocalDate().toString());
                 intent.putExtra("hora", event.getFechaHora().toLocalTime().toString());
